7a310bf347598ca44eb45e5cdc4ffb104ea1ef1b
Timur <toombez.timurtokaev@gmail.com>
2024-04-11 11:41:50 +0300

    Frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,6 +4,7 @@ import Header from "./components/Header";
 import StyleSection from "./components/StyleSection";
 import Footer from "./components/Footer/Footer";
 import RegisterSection from "./components/RegisterSection";
+import LoginSection from "./components/LoginSection";
 
 function App() {
   const [categories, setCategories] = useState("");
@@ -11,29 +12,30 @@ function App() {
 
   const [isLoading, setisLoading] = useState(true);
 
-  useEffect(() => {
-    fetchData();
-  }, []);
+  // useEffect(() => {
+  //   fetchData();
+  // }, []);
 
-  const fetchData = async () => {
-    try {
-      const getCategories = await axios.get(
-        "http://127.0.0.1:8000/api/Category/"
-      );
-      const getThings = await axios.get("http://127.0.0.1:8000/api/Thing/");
+  // const fetchData = async () => {
+  //   try {
+  //     const getCategories = await axios.get(
+  //       "http://127.0.0.1:8000/api/Category/"
+  //     );
+  //     const getThings = await axios.get("http://127.0.0.1:8000/api/Thing/");
 
-      setCategories(getCategories.data);
-      setThings(getThings.data);
-      setisLoading(false);
-      console.log(getThings.data);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+  //     setCategories(getCategories.data);
+  //     setThings(getThings.data);
+  //     setisLoading(false);
+  //     console.log(getThings.data);
+  //   } catch (error) {
+  //     console.log(error);
+  //   }
+  // };
 
   return (
     <>
-      <RegisterSection />
+      <LoginSection />
+      {/* <RegisterSection /> */}
       {/* <Header />
       <StyleSection things={things} isLoading={isLoading} />
       <Footer /> */}
