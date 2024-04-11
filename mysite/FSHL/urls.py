b799b5e06b2826e59d2da794ee67ba163465cae3
Timur <toombez.timurtokaev@gmail.com>
2024-04-11 11:41:41 +0300

    Backend

--- mysite/FSHL/urls.py
+++ mysite/FSHL/urls.py
@@ -1,6 +1,10 @@
 from . import views
 from django.urls import path
 from rest_framework import routers
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 app_name = 'FSHL'
 
@@ -10,6 +14,11 @@ router.register('thing', views.ThingViewSet, basename='Thing')
 router.register('user', views.UserModelViewSet, basename='User')
 # router.register('login', views.IssueToken, basename='Token')
 
-urlpatterns = [path('login', views.issue_token, name='issue_token')]
+
+urlpatterns = [
+    # path('login', views.issue_token, name='issue_token')
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
 
 urlpatterns += router.urls
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     'phonenumber_field',
 
     'FSHL',
+
+    'rest_framework_simplejwt',
 ]
 
 REST_FRAMEWORK = {
@@ -56,6 +58,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
     ]
     
 
